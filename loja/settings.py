aef9db51f1bc8cd62d345768640c227d2e6b9cb8
Eduardo Brandao <eduardo.ads1814@gmail.com>
2022-07-01 20:28:35 -0300

    criando models do perfil de usuarios

--- loja/settings.py
+++ loja/settings.py
@@ -35,6 +35,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'produto',
     'pedido',
+    'perfil',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- perfil/models.py
+++ perfil/models.py
@@ -1,3 +1,59 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Perfil(models.Model):
+    usuario = models.OneToOneField(
+        User, on_delete=models.CASCADE, verbose_name='Usuário')
+    idade = models.PositiveIntegerField()
+    data_nascimento = models.DateField(verbose_name='Data de nascimento')
+    cpf = models.CharField(max_length=11)
+    endereco = models.CharField(max_length=50, verbose_name='Endereço')
+    numero = models.CharField(max_length=5, verbose_name='Número')
+    complemento = models.CharField(max_length=30)
+    bairro = models.CharField(max_length=30)
+    cep = models.CharField(max_length=8)
+    cidade = models.CharField(max_length=30)
+    estado = models.CharField(
+        max_length=2,
+        default='AC',
+        choices=(
+            ('AC', 'Acre'),
+            ('AL', 'Alagoas'),
+            ('AP', 'Amapá'),
+            ('AM', 'Amazonas'),
+            ('BA', 'Bahia'),
+            ('CE', 'Ceará'),
+            ('DF', 'Distrito Federal'),
+            ('ES', 'Espírito Santo'),
+            ('GO', 'Goiás'),
+            ('MA', 'Maranhão'),
+            ('MT', 'Mato Grosso'),
+            ('MS', 'Mato Grosso do Sul'),
+            ('MG', 'Minas Gerais'),
+            ('PA', 'Pará'),
+            ('PB', 'Paraíba'),
+            ('PR', 'Paraná'),
+            ('PE', 'Pernambuco'),
+            ('PI', 'Piauí'),
+            ('RJ', 'Rio de Janeiro'),
+            ('RN', 'Rio Grande do Norte'),
+            ('RS', 'Rio Grande do Sul'),
+            ('RO', 'Rondônia'),
+            ('RR', 'Roraima'),
+            ('SC', 'Santa Catarina'),
+            ('SP', 'São Paulo'),
+            ('SE', 'Sergipe'),
+            ('TO', 'Tocantins'),
+        )
+    )
+
+    def __str__(self):
+        return f'{self.usuario.first_name} {self.usuario.last_name}'
+
+    def clean(self):
+        ...
+
+    class Meta:
+        verbose_name = 'Perfil'
+        verbose_name_plural = 'Perfis'
