e03b1c9c70f680c06c5899fd44e23bb561d6d123
Eduardo Brandao <eduardo.ads1814@gmail.com>
2022-07-01 19:52:30 -0300

    criando models para Pedido e ItemPedido

--- pedido/models.py
+++ pedido/models.py
@@ -1,3 +1,43 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Pedido(models.Model):
+    usuario = models.ForeignKey(
+        User, on_delete=models.CASCADE, verbose_name='Usuário')
+    total = models.FloatField()
+    status = models.CharField(
+        default="C",
+        max_length=1,
+        choices=(
+            ('A', 'Aprovado'),
+            ('C', 'Criado'),
+            ('R', 'Reprovado'),
+            ('P', 'Pendente'),
+            ('E', 'Enviado'),
+            ('F', 'Finalizado'),
+        )
+    )
+
+    def __str__(self):
+        return f'Pedido N. {self.pk}'
+
+
+class ItemPedido(models.Model):
+    pedido = models.ForeignKey(Pedido, on_delete=models.CASCADE)
+    produto = models.CharField(max_length=255)
+    produto_id = models.PositiveIntegerField()
+    variacao = models.CharField(max_length=255, verbose_name='Variação')
+    variacao_id = models.PositiveIntegerField(verbose_name='Variação Id')
+    preco = models.FloatField(verbose_name='Preço')
+    preco_promocional = models.FloatField(
+        default=0, verbose_name='Preço Promocional')
+    quantidade = models.PositiveIntegerField()
+    imagem = models.CharField(max_length=2000)
+
+    def __str__(self):
+        return f'Item do {self.pedido}'
+
+    class Meta:
+        verbose_name = 'Item do pedido'
+        verbose_name_plural = 'Itens do pedido'
