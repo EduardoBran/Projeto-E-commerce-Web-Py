828192dd2a3f69c19c3d290c2bc52dea6d064646
Eduardo Brandao <eduardo.ads1814@gmail.com>
2022-07-02 13:25:08 -0300

    editando campo slug para automatico e exibicao dos prodoutos no admin

--- produto/admin.py
+++ produto/admin.py
@@ -8,6 +8,8 @@ class VariacaoInline(admin.TabularInline):
 
 
 class ProdutoAdmin(admin.ModelAdmin):
+    list_display = ['nome', 'descricao_curta', 'get_preco_formatado',
+                    'get_preco_promocional_formatado']
     inlines = [
         VariacaoInline
     ]
--- produto/models.py
+++ produto/models.py
@@ -3,6 +3,7 @@ from django.db import models
 from django.conf import settings
 from PIL import Image
 import os
+from django.utils.text import slugify
 
 
 class Produto(models.Model):
@@ -12,18 +13,26 @@ class Produto(models.Model):
     descricao_longa = models.TextField(verbose_name='Descrição longa')
     imagem = models.ImageField(
         upload_to='produto_imagens/%Y/%m/', blank=True, null=True)
-    slug = models.SlugField(unique=True)
-    preco_marketing = models.FloatField(verbose_name='Preço marketing')
+    slug = models.SlugField(unique=True, blank=True, null=True)
+    preco_marketing = models.FloatField(verbose_name='Preço')
     preco_marketing_promocional = models.FloatField(
-        default=0, verbose_name='Preço marketing promocional')
+        default=0, verbose_name='Preço Promo. ')
     tipo = models.CharField(
         default='V',
         max_length=1,
         choices=(
-            ('V', 'Variação'),
+            ('V', 'Variável'),
             ('S', 'Simples'),
         )
     )
+
+    def get_preco_formatado(self):
+        return f'R$ {self.preco_marketing:.2f}'.replace('.', ',')
+    get_preco_formatado.short_description = 'Preço'
+
+    def get_preco_promocional_formatado(self):
+        return f'R$ {self.preco_marketing_promocional:.2f}'.replace('.', ',')
+    get_preco_promocional_formatado.short_description = 'Preço Promo.'
 
     @staticmethod
     def resize_image(img, new_width=800):
@@ -45,6 +54,10 @@ class Produto(models.Model):
         )
 
     def save(self, *args, **kwargs):
+        if not self.slug:
+            slug = f'{slugify(self.nome)}'
+            self.slug = slug
+
         super().save(*args, **kwargs)
         max_image_size = 800
 
