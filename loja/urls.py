8d5becdd9676572b1fe2c549a1a0060bf49ebc1b
Eduardo Brandao <eduardo.ads1814@gmail.com>
2022-07-04 11:52:43 -0300

    criando as URLS

--- loja/urls.py
+++ loja/urls.py
@@ -19,6 +19,9 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
+    path('', include('produto.urls')),
+    path('perfil/', include('perfil.urls')),
+    path('pedido/', include('pedido.urls')),
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, documento_root=settings.MEDIA_ROOT)
 
--- pedido/views.py
+++ pedido/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.views.generic.list import ListView
+from django.views import View
+from django.http import HttpResponse
 
-# Create your views here.
+
+class Pagar(View):
+    def get(self, *args, **kwargs):
+        return HttpResponse('Pagar')
+
+
+class FecharPedido(View):
+    def get(self, *args, **kwargs):
+        return HttpResponse('FecharPedido')
+
+
+class Detalhe(View):
+    def get(self, *args, **kwargs):
+        return HttpResponse('Detalhe')
