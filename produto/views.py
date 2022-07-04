732341bf8e601b04392f8d91a9bc421b894a1270
Eduardo Brandao <eduardo.ads1814@gmail.com>
2022-07-04 12:37:42 -0300

    criando arquivo base.html e htmls estaticos

--- produto/views.py
+++ produto/views.py
@@ -2,11 +2,12 @@ from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views import View
 from django.http import HttpResponse
+from . import models
 
 
-class ListaProdutos(View):
-    def get(self, *args, **kwargs):
-        return HttpResponse('Listar')
+class ListaProdutos(ListView):
+    model = models.Produto
+    template_name = 'produto/lista.html'
 
 
 class DetalheProduto(View):
