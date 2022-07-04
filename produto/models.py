cfade0c496cc9baa842b50643586d733d7a1f804
Eduardo Brandao <eduardo.ads1814@gmail.com>
2022-07-04 15:57:41 -0300

    corrigindo a exibicao da formatacao dos precos

--- produto/models.py
+++ produto/models.py
@@ -4,6 +4,7 @@ from django.conf import settings
 from PIL import Image
 import os
 from django.utils.text import slugify
+from utils import utils
 
 
 class Produto(models.Model):
@@ -27,11 +28,11 @@ class Produto(models.Model):
     )
 
     def get_preco_formatado(self):
-        return f'R$ {self.preco_marketing:.2f}'.replace('.', ',')
+        return utils.formata_preco(self.preco_marketing)
     get_preco_formatado.short_description = 'Preço'
 
     def get_preco_promocional_formatado(self):
-        return f'R$ {self.preco_marketing_promocional:.2f}'.replace('.', ',')
+        return utils.formata_preco(self.preco_marketing_promocional)
     get_preco_promocional_formatado.short_description = 'Preço Promo.'
 
     @staticmethod
---
+++ produto/templatetags/omfilters.py
@@ -0,0 +1,9 @@
+from django.template import Library
+from utils import utils
+
+register = Library()
+
+
+@register.filter
+def formata_preco(val):
+    return utils.formata_preco(val=val)
