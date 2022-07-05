07d598d2c7d353930f6e8e36913424265e761a37
Eduardo Brandao <eduardo.ads1814@gmail.com>
2022-07-05 17:43:03 -0300

    adicionando a pagina de detalhes do produto

--- produto/views.py
+++ produto/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 from django.views import View
 from django.http import HttpResponse
 from . import models
@@ -12,9 +13,11 @@ class ListaProdutos(ListView):
     paginate_by = 6
 
 
-class DetalheProduto(View):
-    def get(self, *args, **kwargs):
-        return HttpResponse('Detalhe')
+class DetalheProduto(DetailView):
+    model = models.Produto
+    template_name = 'produto/detalhe.html'
+    context_object_name = 'produto'
+    slug_url_kwarg = 'slug'  # o slug vem da urls.py
 
 
 class AdicionarAoCarrinho(View):
