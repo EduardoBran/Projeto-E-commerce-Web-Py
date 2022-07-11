1f51e5e3423fd57a86e3b8383c5c83a92c24fa02
Eduardo Brandao <eduardo.ads1814@gmail.com>
2022-07-11 15:21:47 -0300

    cadastro de usuarios | parte 1

--- loja/settings.py
+++ loja/settings.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
+    'crispy_forms',
 
 
     # TODO: Remover debub toolbar
@@ -161,6 +161,8 @@ INTERNAL_IPS = [
     '127.0.0.1',
     # ...
 ]
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
