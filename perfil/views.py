7f44491b144326aefe06e85b6d0b57b90366b325
Eduardo Brandao <eduardo.ads1814@gmail.com>
2022-07-13 16:40:48 -0300

    add msgs para usuario ao cadastrar | remocao de links no navbar

--- perfil/views.py
+++ perfil/views.py
@@ -62,6 +62,10 @@ class BasePerfil(View):
 class Criar(BasePerfil):
     def post(self, *args, **kwargs):
         if not self.userform.is_valid() or not self.perfilform.is_valid():
+            messages.error(
+                self.request,
+                'Existem erros no formulário de cadastro. Verifique se todos os campos foram preenchidos corretamente.'
+            )
             return self.renderizar
 
         # Pegando dados
@@ -125,7 +129,7 @@ class Criar(BasePerfil):
             self.request,
             'Você fez login e pode concluir sua compra.'
         )
-        return redirect('perfil:criar')
+        return redirect('produto:carrinho')
 
 
 class Atualizar(View):
