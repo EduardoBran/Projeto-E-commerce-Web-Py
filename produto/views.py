e2310b44cf5c44e54e3d5a7f776ab4a79c17e423
Eduardo Brandao <eduardo.ads1814@gmail.com>
2022-07-14 11:52:53 -0300

    correcao bugs resumodacompra | usuario sem perfil e sem carrinho

--- produto/views.py
+++ produto/views.py
@@ -5,8 +5,7 @@ from django.views import View
 from django.http import HttpResponse
 from django.contrib import messages
 from . import models
-
-from pprint import pprint
+from perfil.models import Perfil
 
 
 class ListaProdutos(ListView):
@@ -25,12 +24,6 @@ class DetalheProduto(DetailView):
 
 class AdicionarAoCarrinho(View):
     def get(self, *args, **kwargs):
-
-        # TODO: Remover linhas abaixo
-        # if self.request.session.get('carrinho'):
-        #     del self.request.session['carrinho']
-        #     self.request.session.save()
-
         # última pág do usuário
         http_referer = self.request.META.get(
             'HTTP_REFERER',
@@ -162,6 +155,22 @@ class ResumoDaCompra(View):
     def get(self, *args, **kwargs):
         if not self.request.user.is_authenticated:
             return redirect('perfil:criar')
+
+        perfil = Perfil.objects.filter(usuario=self.request.user).exists()
+
+        if not perfil:
+            messages.error(
+                self.request,
+                'Usuário sem perfil.'
+            )
+            return redirect('perfil:criar')
+
+        if not self.request.session.get('carrinho'):
+            messages.error(
+                self.request,
+                'Seu carrinho está vazio.'
+            )
+            return redirect('produto:lista')
 
         contexto = {
             'usuario': self.request.user,
