0fa74c4ebcd55c7fbc443276ce27ce2c073fe485
Eduardo Brandao <eduardo.ads1814@gmail.com>
2022-07-18 12:08:13 -0300

    implementado campo de busca

--- produto/views.py
+++ produto/views.py
@@ -6,6 +6,7 @@ from django.http import HttpResponse
 from django.contrib import messages
 from . import models
 from perfil.models import Perfil
+from django.db.models import Q
 
 
 class ListaProdutos(ListView):
@@ -13,6 +14,27 @@ class ListaProdutos(ListView):
     template_name = 'produto/lista.html'
     context_object_name = 'produtos'
     paginate_by = 6
+
+
+class Busca(ListaProdutos):
+    def get_queryset(self, *args, **kwargs):
+        termo = self.request.GET.get('termo') or self.request.session['termo']
+        qs = super().get_queryset(*args, **kwargs)
+
+        if not termo:
+            return qs
+
+        self.request.session['termo'] = termo
+
+        qs = qs.filter(
+            Q(nome__icontains=termo) |
+            Q(descricao_curta__icontains=termo) |
+            Q(descricao_longa__icontains=termo)
+        )
+
+        self.request.session.save()
+
+        return qs
 
 
 class DetalheProduto(DetailView):
