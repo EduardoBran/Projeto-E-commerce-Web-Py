ff3dae8df0e11f796c4bde46b94ee60a45babdd4
Eduardo Brandao <eduardo.ads1814@gmail.com>
2022-07-18 11:39:11 -0300

    cpf unico para cada cadastro

--- perfil/models.py
+++ perfil/models.py
@@ -1,3 +1,4 @@
+from unittest.mock import NonCallableMagicMock
 from django.db import models
 from django.contrib.auth.models import User
 from django.forms import ValidationError
@@ -56,6 +57,16 @@ class Perfil(models.Model):
 
     def clean(self):
         error_messages = {}
+
+        cpf_enviado = self.cpf or None
+        cpf_salvo = None
+        perfil = Perfil.objects.filter(cpf=cpf_enviado).first()
+
+        if perfil:
+            cpf_salvo = perfil.cpf
+
+            if cpf_salvo is not None and self.pk != perfil.pk:
+                error_messages['cpf'] = 'CPF já existe.'
 
         if not valida_cpf(self.cpf):
             error_messages['cpf'] = 'Digite um CPF válido.'
