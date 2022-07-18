4c08b54dbce88448eb64afc29a02a38a1d6c301e
Eduardo Brandao <eduardo.ads1814@gmail.com>
2022-07-18 20:09:52 -0300

    melhoria layout | pag pagar e footer

--- produto/views.py
+++ produto/views.py
@@ -14,6 +14,7 @@ class ListaProdutos(ListView):
     template_name = 'produto/lista.html'
     context_object_name = 'produtos'
     paginate_by = 6
+    ordering = ['-id']
 
 
 class Busca(ListaProdutos):
