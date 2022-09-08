2c518d22bcef9cdad9f6ed971e86b30382041fad
Eduardo Brandao <eduardo.ads1814@gmail.com>
2022-09-08 20:42:14 -0300

    organizando arquivos estaticos

--- loja/settings.py
+++ loja/settings.py
@@ -64,7 +64,7 @@ ROOT_URLCONF = 'loja.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
