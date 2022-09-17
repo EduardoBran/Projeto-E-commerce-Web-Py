441af6b77ecbfbe588079887d52f43283150bc5d
Eduardo Brandao <eduardo.ads1814@gmail.com>
2022-09-16 22:41:03 -0300

    add carousel teste page | edit tamanho botao

--- produto/views.py
+++ produto/views.py
@@ -34,9 +34,9 @@ class Home(ListaProdutos):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['produtosAll'] = Produto.objects.all()
-        # context['produtos1'] = Produto.objects.all()[:3]
-        # context['produtos2'] = Produto.objects.all()[3:6]
-        # context['produtos3'] = Produto.objects.all()[6:9]
+        context['produtos1'] = Produto.objects.all()[:3]
+        context['produtos2'] = Produto.objects.all()[3:6]
+        context['produtos3'] = Produto.objects.all()[6:9]
 
         return context
 
