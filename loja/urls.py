4e825583cfbeed911e9d65602c8c48c16af43899
Eduardo Brandao <eduardo.ads1814@gmail.com>
2022-10-25 11:32:20 -0300

    edit title admin

--- loja/urls.py
+++ loja/urls.py
@@ -25,3 +25,7 @@ urlpatterns = [
     path('suporte/', include('suporte.urls')),
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+admin.site.site_header = 'Unique Store'
+admin.site.site_title = 'Unique Store'
+admin.site.index_title = 'Sistema de Cadastro Unique'
